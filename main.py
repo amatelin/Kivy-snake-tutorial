90932a44bab3a2f7c188d572695f851463f3a8b0
Alexis Matelin <a.matelin@gmail.com>
2015-02-02 14:49:33 -0800

    Commit 2
    Making the skeleton of our app : classes definition

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import kivy
 kivy.require('1.8.0')  # update with your current version
 
+# import the kivy elements used by our classes
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
-class DummyApp(App):
+
+class Playground(Widget):
+    # children widgets containers
+    fruit = ObjectProperty(None)
+    snake = ObjectProperty(None)
+
+
+class Fruit(Widget):
+    pass
+
+
+class Snake(Widget):
+    # children widgets containers
+    head = ObjectProperty(None)
+    tail = ObjectProperty(None)
+
+
+class SnakeHead(Widget):
+    pass
+
+
+class SnakeTail(Widget):
+    pass
+
+
+class SnakeApp(App):
 
     def build(self):
-        return Button(text="Hello World")
+        game = Playground()
+        return game
 
 if __name__ == '__main__':
-    DummyApp().run()
+    SnakeApp().run()

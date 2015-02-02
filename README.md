1320eefb4f2cb959980ed9aa35af4a3e91f096c7
Alexis Matelin <a.matelin@gmail.com>
2015-02-02 07:56:24 -0800

    Initial Commit
    A quick hello world app, just to test if everything works out between buildozer and kivy before we start our Snake

---
+++ README.md
@@ -0,0 +1,4 @@
+Kivy-snake-tutorial
+=========
+
+A simple tutorial intented to explain the basics of kivy, using the classical snake game as example. 
---
+++ main.py
@@ -0,0 +1,13 @@
+import kivy
+kivy.require('1.8.0')  # update with your current version
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class DummyApp(App):
+
+    def build(self):
+        return Button(text="Hello World")
+
+if __name__ == '__main__':
+    DummyApp().run()
